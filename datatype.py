93568c2b1bc4c9b0fc542acd4c52d4bfb6e2e2b6
Praveen733 <heypraveenpk@gmail.com>
2020-11-22 22:07:51 +0530

    Program to print data type of given data

---
+++ datatype.py
@@ -0,0 +1,9 @@
+datatype=input("Enter the data:")
+if datatype.isalpha():
+    print("Entered data is string")
+elif datatype.isalnum:
+    print("Entered data is alphanumeric")
+elif datatype.isdigit():
+    print("Entered data is digit")
+else:
+    print("_____Invalid input___")

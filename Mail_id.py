b9c4c6614becc4eabe80d81dc845102703b0ea50
Praveen733 <heypraveenpk@gmail.com>
2020-11-29 22:49:27 +0530

    Program to print mail id suggestions for a given name

---
+++ Mail_id.py
@@ -0,0 +1,9 @@
+def suggestion(name):
+    return name.replace(" ","")
+name=input("Enter your name:")
+a=name+"@gmail.com"
+b=name+"123@gmail.com"
+print("Your Mail id suggestions are")
+print(a)
+print(b)
+suggestion(name)

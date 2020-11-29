86c7a7abc557c8fcd8cb4f643ee7953987160424
Praveen733 <heypraveenpk@gmail.com>
2020-11-29 22:53:41 +0530

    Program to print entered year is leap year or not

---
+++ leap_year.py
@@ -0,0 +1,12 @@
+year=int(input("Enter the year:"))
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("{} is a leap year".format(year))
+        else:
+            print("{} is a not leap year".format(year))
+    else:
+        print("{} is a leap year".format(year))
+else:
+    print("{} is not a leap year".format(year))
+

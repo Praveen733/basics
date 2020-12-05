8e8e55d28d009590dae2b95052bb03da61a52342
Praveen733 <heypraveenpk@gmail.com>
2020-12-05 22:48:08 +0530

    Love calculator program

---
+++ love_calculator.py
@@ -0,0 +1,18 @@
+import random
+while True:
+   name=input("Enter your name:")
+   name2=input("enter your crush name:")
+   print(":)___LOVE CALCULATOR___:)")
+   love=random.randint(70,100)
+   print("Your love  is", love, "%")
+   if love >=90:
+       print("Your love is divine")
+   elif love>=80:
+       print("Your love is good")
+   elif love>=70:
+       print("Your love is worst,find a beter girl:")
+   a=input("do you want to continue:")
+   if a=="yes":
+       continue
+   else:
+       break

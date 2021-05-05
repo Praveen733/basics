51fd841ab05e8119f96560eb31c37b49fee53a46
Praveen <heypraveenpk@gmail.com>
2021-05-05 13:28:51 +0530

    Python function student(). Using function attributes display the names of all arguments

---
+++ student.py
@@ -0,0 +1,3 @@
+def student(student_id,student_name,student_class):
+    return  f" Student Id:{student_id}\n Student Name:{student_name} \n Student Class:{student_class}"
+print(student('Abc123','Praveen','Bca'))
